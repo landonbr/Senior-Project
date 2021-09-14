a21a4676a1f11e5da66d77272019d97a0ce1e40e
landonbr <landonbrown0797@gmail.com>
2021-09-14 10:25:55 -0400

    brokern servo :(

--- Servo/Servo_test/Servo_test.ino
+++ Servo/Servo_test/Servo_test.ino
@@ -4,9 +4,9 @@ Servo myservo;  // create servo object to control a servo
 
 #define servoPin 9
 
-/* variables */
-int start = 17;     // starting position of servo
-int angle = 45;     // turn angle (i.e pin ON)
+/* variables for broken servo */
+int start = 55;     // starting position of servo 17
+int angle = 100;     // turn angle (i.e pin ON)
 int pos = 0;        // variable to store the servo position
 int overshoot = 5;  // reset state of pin after angle rotationi (i.e pin OFF)
 
@@ -19,7 +19,7 @@ void rotate() {
   // 0 degrees to (45+5) in steps of 1 degree per 15ms
   for (pos = start; pos <= angle+overshoot; pos += 1) { 
     myservo.write(pos);   
-    delay(15);    // speed of rotation - 15ms
+    delay(15);    // speed of rotation - 15m s
   }
 }
 
@@ -36,4 +36,3 @@ void loop() {
   //myservo.write(start);
   //delay(10000);
 //}
-
